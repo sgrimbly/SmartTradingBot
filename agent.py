46da06550da7559c537e4d0f236df07a6bddab17
St John Grimbly <stjohngrimbly@protonmail.com>
2021-05-01 14:37:14 +0200

    Update agent code. Add utils.

--- agent.py
+++ agent.py
@@ -2,21 +2,29 @@
 # 1 May 2021
 # Partially inspired by https://github.com/pskrunner14/trading-bot
 
+import random
 from typing import List, Union
 from collections import deque
 
 import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.models import clone_model, load_model
 from tensorflow.keras.layers import Dense
-
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import Loss, MeanSquaredError
 class DQNAgent:
     """This is core logic for the agent."""
 
     def __init__(
         self, 
         state_dim: int, 
-        hidden_layer_sizes = [128,256,256,128]: List,
-        activation = "relu": str,
-        model_name = Union[None, str]: str,
+        hidden_layer_sizes: List = [128,256,256,128],
+        activation: str = "relu",
+        discount: float = 0.99,
+        model_name: Union[None, str] = None,
+        target_update_freq: int = 10e3,
+        learning_rate: float = 1e-3,
+        loss: Loss = MeanSquaredError,
     ) -> None:
         """Initialise instance of a DQN agent.
             args:
@@ -26,20 +34,27 @@ class DQNAgent:
         self._state_dim = state_dim 
         self._hidden_layer_sizes = hidden_layer_sizes
         self._activation = activation
+        self._discount = discount
         self._model_name = model_name
-
-        self.memory = deque(maxlen=10e4)
+        self._target_update_freq = target_update_freq
+        self._learning_rate = learning_rate
+        self._loss = loss
+        
+        self._optimizer = Adam(self._learning_rate)
+        self._target_trailing_by = 0
+        self.memory = deque(maxlen=1000)
         self.num_actions = 3 # Agents can hold, buy, or sell.
         if self._model_name is not None:
             self.model = self._load()
         else:
-            self.model = self._model()
+            self.model = self._q_network()
+        self.target_model = clone_model(self.model)
     
-    def _load_model(self) -> tf.Model:
+    def _load_model(self) -> Model:
         """Load a pretrained model if specified."""
-        return self._load_model("models/" + self.model_name, custom_objects=self.custom_objects)
+        return self.load_model("models/" + self.model_name, custom_objects=self.custom_objects)
 
-    def _model(self) -> tf.Model:
+    def _q_network(self) -> Model:
         """Create the neural network architecture for the DQN agent."""
         model = tf.keras.Sequential()
         model.add(Dense(units=self._hidden_layer_sizes[0], 
@@ -50,6 +65,7 @@ class DQNAgent:
             model.add(Dense(self._hidden_layer_sizes[i], activation=self._activation))
 
         model.add(Dense(self.num_actions))
+        model.compile(loss=self._loss, optimizer=self._optimizer)
         return model
 
     def add_memory(
@@ -63,4 +79,17 @@ class DQNAgent:
         """Add memory to replay buffer."""
         self.memory.append((state, action, reward, next_state, done))
 
-    
+    def train(self, batch_size: int = 32) -> None:
+        if self._target_trailing_by % self._target_update_freq == 0:
+            self.target_model.set_weights(self.model.get_weights())
+        
+        batch = random.sample(self.memory, batch_size)
+
+        for state, action, reward, next_state, done in batch:
+            if done:
+                target = reward
+            else:
+                target = reward + self._discount*tf.argmax(self.target_model(next_state)[0])
+
+    #def save(self, episode):
+    #    self.model.save("models/{}_{}".format(self.model_name, episode))
---
+++ utils.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+from datetime import date
+
+import yfinance as yf
+
+"""Utility functions used by other files."""
+
+def get_data(
+    stock, 
+    start_date, 
+    end_date=None, 
+    test_split=0.8,
+    data_type="Adj Close"
+) -> Tuple:
+    """Get the Yahoo data for a given stock in a specified date range."""
+    if end_date is None:
+        end_date = date.today().strftime("%Y-%m-%d")
+    # TODO Check for cached data
+
+    # auto_adjust=True -> removing Adjust Close column
+    # actions =”inline” -> dowload dividend and splits of stock
+    # ['FB','AAPL',...] -> download data for multiple stocks
+    df_yahoo = yf.download(stock, start=start_date, end=end_date, progress=False)
+    data = df_yahoo[data_type]
+    test_split_index = int(test_split*len(data)) # Keep 20% of data for validation
+    return data[:test_split_index], data[test_split_index:]
